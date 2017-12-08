eedc4d8d28477058b94ef32eef9cdf2e77b02096
ChrisPettinari <chrispettinari@gmail.com>
2017-12-08 13:09:56 -0500

    eh v. whatever

--- MarkovTextGenerator/Program.cs
+++ MarkovTextGenerator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MarkovTextGenerator
 {
@@ -11,17 +12,49 @@ namespace MarkovTextGenerator
         static void Main(string[] args)
         {
             Chain chain = new Chain();
+            String line;
+            const Int32 BufferSize = 128;
+            using (var fileStream = File.OpenRead(@"C:\Users\eahscs\Desktop\spicy.txt"))
+            using (var sr = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+
+
+             
+                //Pass the file path and file name to the StreamReader constructor
+               
+
+                //Read the first line of text
+
+
+                //Continue to read until you reach end of file
+                while ((line = sr.ReadLine()) != null)
+                {
+                    //write the lie to console window
+                    chain.AddString(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+                //close the file
+                sr.Close();
+                
+            }
+            
+            
 
             Console.WriteLine("Welcome to Marky Markov's Random Text Generator!");
 
             Console.WriteLine("Enter some text I can learn from (enter single ! to finish): ");
+
+            foreach(Chain in Chain)
+            Console.WriteLine();
 
             while (true)
             {
 
                 Console.Write("> ");
 
-                String line = Console.ReadLine();
+                line = Console.ReadLine();
                 if (line == "!")
                     break;
 
